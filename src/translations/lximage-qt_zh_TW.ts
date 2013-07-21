6174e71b389162d8f1b6bfbaba76799d8b047351
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2013-07-22 01:16:09 +0800

    Add zh_TW translation and a template ts file.

---
+++ src/translations/lximage-qt_zh_TW.ts
@@ -0,0 +1,384 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>LxImage::Application</name>
+    <message>
+        <location filename="../application.cpp" line="105"/>
+        <source>Take a screenshot</source>
+        <translation>拍攝螢幕快照</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="106"/>
+        <source>[FILE1, FILE2,...]</source>
+        <translation>[檔案1、檔案2...]</translation>
+    </message>
+</context>
+<context>
+    <name>LxImage::MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="105"/>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="106"/>
+        <source>LXImage - a simple and fast image viewer
+
+Copyright (C) 2013
+LXDE Project: http://lxde.org/
+
+Authors:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>LXImage - 簡單快速的圖片檢視程式r
+
+版權所有 (C) 2013
+LXDE 專案: http://lxde.org/
+
+Authors:
+洪任諭 (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="194"/>
+        <source>Open File</source>
+        <translation>開啟檔案</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="195"/>
+        <location filename="../mainwindow.cpp" line="216"/>
+        <source>Image files (%1)</source>
+        <translation>影像檔案 (%1)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="216"/>
+        <source>Save File</source>
+        <translation>儲存檔案</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="395"/>
+        <source>%1 (Loading...) - Image Viewer</source>
+        <translation>%1 (正在載入...) - 圖片檢視程式</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="400"/>
+        <source>%1 (Failed to Load) - Image Viewer</source>
+        <translation>%1 (載入失敗) - 圖片檢視程式</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="404"/>
+        <source>%1 (%2x%3) - Image Viewer</source>
+        <translation>%1 (%2x%3) - 圖片檢視程式</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="414"/>
+        <source>Image Viewer</source>
+        <translation>圖片檢視程式</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="366"/>
+        <source>Image Viewer</source>
+        <translation>圖片檢視程式</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="367"/>
+        <source>&amp;About</source>
+        <translation>關於(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="368"/>
+        <source>&amp;Open File</source>
+        <translation>開啟檔案(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="369"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="370"/>
+        <source>&amp;Save</source>
+        <translation>儲存(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="371"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="372"/>
+        <source>Save &amp;As</source>
+        <translation>另存新檔(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="373"/>
+        <source>Ctrl+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="374"/>
+        <source>&amp;Close</source>
+        <translation>關閉(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="375"/>
+        <source>Ctrl+W</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="376"/>
+        <source>Zoom &amp;In</source>
+        <translation>拉近(&amp;I)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="377"/>
+        <source>Ctrl++</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="378"/>
+        <source>Zoomo &amp;Out</source>
+        <translation>拉遠(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="379"/>
+        <source>Ctrl+-</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="380"/>
+        <source>&amp;Copy to Clipboard</source>
+        <translation>複製到剪貼簿(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="381"/>
+        <location filename="../../build/src/ui_mainwindow.h" line="383"/>
+        <source>Next File</source>
+        <translation>下一個檔案</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="385"/>
+        <source>Right</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="386"/>
+        <location filename="../../build/src/ui_mainwindow.h" line="388"/>
+        <source>Previous File</source>
+        <translation>前一個檔案</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="390"/>
+        <source>Left</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="391"/>
+        <source>Original Size</source>
+        <translation>原尺寸</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="392"/>
+        <source>&amp;Fit</source>
+        <translation>適合視窗大小(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="393"/>
+        <source>&amp;Rotate Clockwise</source>
+        <translation>順時鐘旋轉(&amp;R)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="394"/>
+        <source>Rotate &amp;Counterclockwise</source>
+        <translation>逆時鐘旋轉(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="395"/>
+        <source>P&amp;references</source>
+        <translation>偏好設定(&amp;R)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="396"/>
+        <source>&amp;Print</source>
+        <translation>列印(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="397"/>
+        <source>First File</source>
+        <translation>第一個檔案</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="398"/>
+        <source>Home</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="399"/>
+        <source>Last File</source>
+        <translation>最後一個檔案</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="400"/>
+        <source>End</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="401"/>
+        <source>&amp;New Window</source>
+        <translation>新視窗(&amp;N)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="402"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="403"/>
+        <source>Flip &amp;Horizontally</source>
+        <translation>水平翻轉(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="404"/>
+        <source>Capture Screenshot</source>
+        <translation>拍攝螢幕快照</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="405"/>
+        <source>F&amp;ull Screen</source>
+        <translation>全螢幕(&amp;U)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="406"/>
+        <source>F11</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="407"/>
+        <source>Flip &amp;Vertically</source>
+        <translation>垂直翻轉(&amp;V)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="408"/>
+        <source>&amp;Paste from Clipboard</source>
+        <translation>從剪貼簿貼上(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="409"/>
+        <source>&amp;Slide Show</source>
+        <translation>投影片播放(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="410"/>
+        <source>&amp;Delete</source>
+        <translation>刪除(&amp;D)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="411"/>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="412"/>
+        <source>&amp;File</source>
+        <translation>檔案(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="413"/>
+        <source>&amp;Help</source>
+        <translation>說明(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="414"/>
+        <source>Go</source>
+        <translation>前往</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="415"/>
+        <source>&amp;View</source>
+        <translation>檢視(&amp;V)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="416"/>
+        <source>&amp;Edit</source>
+        <translation>編輯(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="417"/>
+        <source>toolBar</source>
+        <translation>工具列</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../../build/src/ui_preferencedialog.h" line="80"/>
+        <source>Preferences</source>
+        <translation>偏好設定</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencedialog.h" line="81"/>
+        <source>TODO: Add configuration options here</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencedialog.h" line="82"/>
+        <source>Tab 1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencedialog.h" line="83"/>
+        <source>Tab 2</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotDialog</name>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="119"/>
+        <source>Screenshot</source>
+        <translation>螢幕快照</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="120"/>
+        <source>Take a screenshot</source>
+        <translation>拍攝螢幕快照</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="121"/>
+        <source>Region</source>
+        <translation>區域</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="122"/>
+        <source>Whole screen</source>
+        <translation>整個螢幕</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="123"/>
+        <source>Current window only</source>
+        <translation>只有目前的視窗</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="124"/>
+        <source>Include mouse cursor</source>
+        <translation>包含滑鼠游標</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="125"/>
+        <source> seconds</source>
+        <translation>秒</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="126"/>
+        <source>Delay:</source>
+        <translation>延遲:</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="127"/>
+        <source>Include window title and frame</source>
+        <translation>包含視窗標題和邊框</translation>
+    </message>
+</context>
+</TS>
