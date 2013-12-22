d9533b2c1bfd99bc15e71d86702722470e6929e0
zvacet <ikoli@yahoo.com>
2013-12-22 00:41:29 +0000

    Commit from LXDE Pootle server by user zvacet.: 50 of 70 strings translated (0 fuzzy).

--- src/translations/lximage-qt_template-hr.ts
+++ src/translations/lximage-qt_template-hr.ts
@@ -5,12 +5,12 @@
     <message>
         <location filename="../application.cpp" line="105"/>
         <source>Take a screenshot</source>
-        <translation type="unfinished"/>
+        <translation>Napravi sliku zaslona</translation>
     </message>
     <message>
         <location filename="../application.cpp" line="106"/>
         <source>[FILE1, FILE2,...]</source>
-        <translation type="unfinished"/>
+        <translation>[DATOTEKA1, DATOTEKA2,...]</translation>
     </message>
 </context>
 <context>
@@ -18,7 +18,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="105"/>
         <source>About</source>
-        <translation type="unfinished"/>
+        <translation>O programu</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="106"/>
@@ -29,43 +29,49 @@ LXDE Project: http://lxde.org/
 
 Authors:
 Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
-        <translation type="unfinished"/>
+        <translation>LXImage - jednostavni i brzi preglednik slika
+
+Autorska prava (C) 2013
+LXDE Project: http://lxde.org/
+
+Autori:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="194"/>
         <source>Open File</source>
-        <translation type="unfinished"/>
+        <translation>Otvori datoteku</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="195"/>
         <location filename="../mainwindow.cpp" line="216"/>
         <source>Image files (%1)</source>
-        <translation type="unfinished"/>
+        <translation>Datoteke slika (%1)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="216"/>
         <source>Save File</source>
-        <translation type="unfinished"/>
+        <translation>Spremi datoteku</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="395"/>
         <source>%1 (Loading...) - Image Viewer</source>
-        <translation type="unfinished"/>
+        <translation>%1 (Učitavam...) - Preglednik slika</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="400"/>
         <source>%1 (Failed to Load) - Image Viewer</source>
-        <translation type="unfinished"/>
+        <translation>%1 (neuspjelo učitavanje) - Pregledmik slika</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="404"/>
         <source>%1 (%2x%3) - Image Viewer</source>
-        <translation type="unfinished"/>
+        <translation>%1 (%2x%3) - Preglednik slika</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="414"/>
         <source>Image Viewer</source>
-        <translation type="unfinished"/>
+        <translation>Preglednik slika</translation>
     </message>
 </context>
 <context>
@@ -73,52 +79,52 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="366"/>
         <source>Image Viewer</source>
-        <translation type="unfinished"/>
+        <translation>Preglednik slika</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="367"/>
         <source>&amp;About</source>
-        <translation type="unfinished"/>
+        <translation>&amp;O programu</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="368"/>
         <source>&amp;Open File</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Otvori datoteku</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="369"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"/>
+        <translation>Ctrl+O</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="370"/>
         <source>&amp;Save</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Spremi</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="371"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"/>
+        <translation>Ctrl+S</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="372"/>
         <source>Save &amp;As</source>
-        <translation type="unfinished"/>
+        <translation>Spremi &amp;kao</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="373"/>
         <source>Ctrl+A</source>
-        <translation type="unfinished"/>
+        <translation>Ctrl+A</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="374"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Zatvori</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="375"/>
         <source>Ctrl+W</source>
-        <translation type="unfinished"/>
+        <translation>Ctrl+W</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="376"/>
@@ -128,7 +134,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="377"/>
         <source>Ctrl++</source>
-        <translation type="unfinished"/>
+        <translation>Ctrl++</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="378"/>
@@ -138,7 +144,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="379"/>
         <source>Ctrl+-</source>
-        <translation type="unfinished"/>
+        <translation>Ctrl+-</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="380"/>
@@ -149,23 +155,23 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
         <location filename="../../build/src/ui_mainwindow.h" line="381"/>
         <location filename="../../build/src/ui_mainwindow.h" line="383"/>
         <source>Next File</source>
-        <translation type="unfinished"/>
+        <translation>Slijedeća datoteka</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="385"/>
         <source>Right</source>
-        <translation type="unfinished"/>
+        <translation>Desno</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="386"/>
         <location filename="../../build/src/ui_mainwindow.h" line="388"/>
         <source>Previous File</source>
-        <translation type="unfinished"/>
+        <translation>Prethodna datoteka</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="390"/>
         <source>Left</source>
-        <translation type="unfinished"/>
+        <translation>Lijevo</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="391"/>
@@ -240,12 +246,12 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="405"/>
         <source>F&amp;ull Screen</source>
-        <translation type="unfinished"/>
+        <translation>C&amp;ijeli zaslon</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="406"/>
         <source>F11</source>
-        <translation type="unfinished"/>
+        <translation>F11</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="407"/>
@@ -260,47 +266,47 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="409"/>
         <source>&amp;Slide Show</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Klizni prikaz</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="410"/>
         <source>&amp;Delete</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Izbriši</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="411"/>
         <source>Del</source>
-        <translation type="unfinished"/>
+        <translation>Del</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="412"/>
         <source>&amp;File</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Datoteka</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="413"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Pomoć</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="414"/>
         <source>Go</source>
-        <translation type="unfinished"/>
+        <translation>Idi</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="415"/>
         <source>&amp;View</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Pogled</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="416"/>
         <source>&amp;Edit</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Uredi</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_mainwindow.h" line="417"/>
         <source>toolBar</source>
-        <translation type="unfinished"/>
+        <translation>alatna traka</translation>
     </message>
 </context>
 <context>
@@ -308,7 +314,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_preferencedialog.h" line="80"/>
         <source>Preferences</source>
-        <translation type="unfinished"/>
+        <translation>Osobitosti</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_preferencedialog.h" line="81"/>
@@ -318,12 +324,12 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_preferencedialog.h" line="82"/>
         <source>Tab 1</source>
-        <translation type="unfinished"/>
+        <translation>Kartica 1</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_preferencedialog.h" line="83"/>
         <source>Tab 2</source>
-        <translation type="unfinished"/>
+        <translation>Kartica 2</translation>
     </message>
 </context>
 <context>
@@ -331,47 +337,47 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="119"/>
         <source>Screenshot</source>
-        <translation type="unfinished"/>
+        <translation>Snimka zaslona</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="120"/>
         <source>Take a screenshot</source>
-        <translation type="unfinished"/>
+        <translation>Napravi snimku zaslona</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="121"/>
         <source>Region</source>
-        <translation type="unfinished"/>
+        <translation>Regija</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="122"/>
         <source>Whole screen</source>
-        <translation type="unfinished"/>
+        <translation>Cijeli zaslon</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="123"/>
         <source>Current window only</source>
-        <translation type="unfinished"/>
+        <translation>Samo trenutni prozor</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="124"/>
         <source>Include mouse cursor</source>
-        <translation type="unfinished"/>
+        <translation>uključi pokazivač miša</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="125"/>
         <source> seconds</source>
-        <translation type="unfinished"/>
+        <translation> sekundi</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="126"/>
         <source>Delay:</source>
-        <translation type="unfinished"/>
+        <translation>Odgoda:</translation>
     </message>
     <message>
         <location filename="../../build/src/ui_screenshotdialog.h" line="127"/>
         <source>Include window title and frame</source>
-        <translation type="unfinished"/>
+        <translation>Uključi naslov prozora i okvir</translation>
     </message>
 </context>
 </TS>
