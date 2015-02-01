47b4fc59ba4ab097e8875c9d9fa141bdf377c087
Stef Binde <stef.binde@riseup.net>
2015-02-01 10:49:32 +0100

    Create lximage-qt_it.ts
    Created italian language file, with accelerators.

---
+++ src/translations/lximage-qt_it.ts
@@ -0,0 +1,417 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it" sourcelanguage="it">
+<context>
+    <name>LxImage::Application</name>
+    <message>
+        <location filename="../application.cpp" line="112"/>
+        <source>Take a screenshot</source>
+        <translation>Cattura una schermata</translation>
+    </message>
+    <message>
+        <location filename="../application.cpp" line="113"/>
+        <source>[FILE1, FILE2,...]</source>
+        <translation>[File1, File2,...]</translation>
+    </message>
+</context>
+<context>
+    <name>LxImage::MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="144"/>
+        <source>About</source>
+        <translation>A proposito</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="145"/>
+        <source>LXImage - a simple and fast image viewer
+
+Copyright (C) 2013
+LXDE Project: http://lxde.org/
+
+Authors:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translatorcomment>need update...</translatorcomment>
+        <translation type="unfinished">LXImage - un visualizzatore immagini semplice e veloce
+
+Copyright (C) 2013
+LXDE Project: http://lxde.org/
+
+Autor:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="238"/>
+        <source>Open File</source>
+        <translation>Apri file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="239"/>
+        <location filename="../mainwindow.cpp" line="260"/>
+        <source>Image files (%1)</source>
+        <translation>File immagini (%1)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="260"/>
+        <source>Save File</source>
+        <translation>Salva file</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="516"/>
+        <source>%1 (Loading...) - Image Viewer</source>
+        <translation>%1 (Caricando...) - Visualizzatore immagini</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="521"/>
+        <source>%1 (Failed to Load) - Image Viewer</source>
+        <translation>%1 (Errore in caricamento) - Visualizzatore immagini</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="525"/>
+        <source>%1 (%2x%3) - Image Viewer</source>
+        <translation>%1 (%2x%3) - Visualizzatore Immagini</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="535"/>
+        <source>Image Viewer</source>
+        <translation>Visualizzatore immagini</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="710"/>
+        <source>Thumbnails</source>
+        <translation>Miniature</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="412"/>
+        <source>Image Viewer</source>
+        <translation>Visualizzatore immagini</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="413"/>
+        <source>&amp;About</source>
+        <translation>A &amp;proposito</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="414"/>
+        <source>&amp;Open File</source>
+        <translation>&amp;Apri</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="415"/>
+        <source>Ctrl+O</source>
+        <translatorcomment>Shortcuts werden automatisch übersetzt</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="416"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Salva</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="417"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="418"/>
+        <source>Save &amp;As</source>
+        <translation>Salva &amp;come</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="419"/>
+        <source>Ctrl+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="420"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Chiudi</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="421"/>
+        <source>Ctrl+W</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="422"/>
+        <source>Zoom &amp;In</source>
+        <translation>&amp;Ingrandisci</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="423"/>
+        <source>Ctrl++</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="424"/>
+        <source>Zoom &amp;Out</source>
+        <translation>&amp;Rimpicciolisci</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="425"/>
+        <source>Ctrl+-</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="426"/>
+        <source>&amp;Copy to Clipboard</source>
+        <translation>&amp;Copia negli appunti</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="427"/>
+        <location filename="../../build/src/ui_mainwindow.h" line="429"/>
+        <source>Next File</source>
+        <translation>Prossimo</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="432"/>
+        <location filename="../../build/src/ui_mainwindow.h" line="434"/>
+        <source>Previous File</source>
+        <translation>Precedente</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="431"/>
+        <source>PgDown</source>
+        <translation>Pagina giù</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="436"/>
+        <source>PgUp</source>
+        <translation>Pagina sù</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="437"/>
+        <source>Original Size</source>
+        <translation type="unfinished">&amp;Dimensioni originiali</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="438"/>
+        <source>&amp;Fit</source>
+        <translation>&amp;Adatta alla finestra</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="439"/>
+        <source>&amp;Rotate Clockwise</source>
+        <translation>Gira a &amp;destra</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="440"/>
+        <source>Rotate &amp;Counterclockwise</source>
+        <translation>Gira a &amp;sinistra</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="441"/>
+        <source>P&amp;references</source>
+        <translation>&amp;Preferenze</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="442"/>
+        <source>&amp;Print</source>
+        <translation>&amp;Stampa</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="443"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="444"/>
+        <source>First File</source>
+        <translation>Primo</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="445"/>
+        <source>Home</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="446"/>
+        <source>Last File</source>
+        <translation>Ultimo</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="447"/>
+        <source>End</source>
+        <translation>Fine</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="448"/>
+        <source>&amp;New Window</source>
+        <translation>&amp;Nuova finestra</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="449"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="450"/>
+        <source>Flip &amp;Horizontally</source>
+        <translation>Rispecchia &amp;horizzontalmente</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="451"/>
+        <source>Capture Screenshot</source>
+        <translation>Cattura schermata</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="452"/>
+        <source>F&amp;ull Screen</source>
+        <translation>&amp;Schermo intero</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="453"/>
+        <source>F11</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="454"/>
+        <source>Flip &amp;Vertically</source>
+        <translation>Rispecchia &amp;verticalmente</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="455"/>
+        <source>&amp;Paste from Clipboard</source>
+        <translation>&amp;Incolla da appunti</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="456"/>
+        <source>&amp;Slide Show</source>
+        <translation>&amp;Presentazione</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="457"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Cestina</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="458"/>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="459"/>
+        <source>Show Thumbnails</source>
+        <translation type="unfinished">&amp;Mostra barra miniature</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="460"/>
+        <source>File Properties</source>
+        <translatorcomment>Übersetzung von &apos;file&apos; weggelassen, da redundant.</translatorcomment>
+        <translation>Proprietà</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="461"/>
+        <source>&amp;File</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="462"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aiuto</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="463"/>
+        <source>Go</source>
+        <translation>Vai</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="464"/>
+        <source>&amp;View</source>
+        <translation>&amp;Visualizza</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="465"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Modifica</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_mainwindow.h" line="466"/>
+        <source>Toolbar</source>
+        <translation>Barra degli strumenti</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../../build/src/ui_preferencesdialog.h" line="125"/>
+        <source>Preferences</source>
+        <translation>Preferenze</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencesdialog.h" line="126"/>
+        <source>Icon theme:</source>
+        <translation>Tema icone:</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencesdialog.h" line="127"/>
+        <source>Normal background color:</source>
+        <translation>Colore sfondo:</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencesdialog.h" line="128"/>
+        <source>Fullscreen background color:</source>
+        <translation>Colore sfondo schermo intero:</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencesdialog.h" line="131"/>
+        <source>Slide show interval (seconds):</source>
+        <translation>Ritardo presentazione (secondi):</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_preferencesdialog.h" line="132"/>
+        <source>General</source>
+        <translation>Generali</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotDialog</name>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="123"/>
+        <source>Screenshot</source>
+        <translation>Schermata</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="124"/>
+        <source>Take a screenshot</source>
+        <translation>Catturta schermata</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="125"/>
+        <source>Region</source>
+        <translation>Regione</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="126"/>
+        <source>Whole screen</source>
+        <translation>Schermo intero</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="127"/>
+        <source>Current window only</source>
+        <translation>Finestra attuale</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="128"/>
+        <source>Include mouse cursor</source>
+        <translation>Includi cursore</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="129"/>
+        <source> seconds</source>
+        <translation> secondi</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="130"/>
+        <source>Delay:</source>
+        <translation>Ritardo:</translation>
+    </message>
+    <message>
+        <location filename="../../build/src/ui_screenshotdialog.h" line="131"/>
+        <source>Include window title and frame</source>
+        <translation>Includi cornice finestra</translation>
+    </message>
+</context>
+</TS>
