459135d0e9d639ed315da7d868d9e424c92777b0
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-10 18:45:05 +0200

    Translate lximage-qt_ca.ts (Catalan)

--- lximage-qt_ca.ts
+++ lximage-qt_ca.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../../application.cpp" line="89"/>
         <source>Take a screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Pren una captura de pantalla</translation>
     </message>
     <message>
         <location filename="../../application.cpp" line="93"/>
         <source>[FILE1, FILE2,...]</source>
-        <translation type="unfinished"></translation>
+        <translation>[FITXER1, FITXER2,...]</translation>
     </message>
 </context>
 <context>
@@ -19,7 +19,7 @@
     <message>
         <location filename="../../mainwindow.cpp" line="148"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Quant a</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="149"/>
@@ -30,48 +30,54 @@ LXDE Project: http://lxde.org/
 
 Authors:
 Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>LXImage - un visualitzador d&apos;imatges senzill i ràpid
+
+Copyright (C) 2013
+Projecte LXDE: http://lxde.org/
+
+Autors:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="242"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Obre un fitxer</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="243"/>
         <location filename="../../mainwindow.cpp" line="264"/>
         <source>Image files (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation>Fitxers d&apos;imatges (%1)</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="264"/>
         <source>Save File</source>
-        <translation type="unfinished"></translation>
+        <translation>Desa el fitxer</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="530"/>
         <source>[*]%1 (Loading...) - Image Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>[*]%1 (s&apos;està carregant...) - Visualitzador d&apos;imatges</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="535"/>
         <source>[*]%1 (Failed to Load) - Image Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>[*]%1 (ha fallat la càrrega) - Visualitzador d&apos;imatges</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="539"/>
         <source>[*]%1 (%2x%3) - Image Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>[*]%1 (%2x%3) - Visualitzador d&apos;imatges</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="576"/>
         <source>[*]Image Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>[*]Visualitzador d&apos;imatges</translation>
     </message>
     <message>
         <location filename="../../mainwindow.cpp" line="819"/>
         <source>Thumbnails</source>
-        <translation type="unfinished"></translation>
+        <translation>Miniatures</translation>
     </message>
 </context>
 <context>
@@ -79,249 +85,249 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../mainwindow.ui" line="14"/>
         <source>Image Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>Visualitzador d&apos;imatges</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="159"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>Qu&amp;ant a</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="169"/>
         <source>&amp;Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Obre un fitxer</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="172"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+O</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="182"/>
         <source>&amp;Save</source>
-        <translation type="unfinished"></translation>
+        <translation>De&amp;sa</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="185"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+S</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="195"/>
         <source>Save &amp;As</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Anomena i desa</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="198"/>
         <source>Ctrl+A</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+A</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="208"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"></translation>
+        <translation>Tan&amp;ca</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="211"/>
         <source>Ctrl+W</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+W</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="221"/>
         <source>Zoom &amp;In</source>
-        <translation type="unfinished"></translation>
+        <translation>Ampl&amp;ia el zoom</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="224"/>
         <source>Ctrl++</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl++</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="234"/>
         <source>Zoom &amp;Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Redueix el z&amp;oom</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="237"/>
         <source>Ctrl+-</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+-</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="247"/>
         <source>&amp;Copy to Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Copia al porta-retalls</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="257"/>
         <location filename="../../mainwindow.ui" line="260"/>
         <source>Next File</source>
-        <translation type="unfinished"></translation>
+        <translation>Fitxer següent</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="273"/>
         <location filename="../../mainwindow.ui" line="276"/>
         <source>Previous File</source>
-        <translation type="unfinished"></translation>
+        <translation>Fitxer anterior</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="263"/>
         <source>PgDown</source>
-        <translation type="unfinished"></translation>
+        <translation>Re Pàg</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="279"/>
         <source>PgUp</source>
-        <translation type="unfinished"></translation>
+        <translation>Av Pàg</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="289"/>
         <source>Original Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Mida original</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="299"/>
         <source>&amp;Fit</source>
-        <translation type="unfinished"></translation>
+        <translation>A&amp;justa</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="309"/>
         <source>&amp;Rotate Clockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Gi&amp;ra en sentit horari</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="319"/>
         <source>Rotate &amp;Counterclockwise</source>
-        <translation type="unfinished"></translation>
+        <translation>Gira en sentit anti&amp;horari</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="324"/>
         <source>P&amp;references</source>
-        <translation type="unfinished"></translation>
+        <translation>P&amp;referències</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="329"/>
         <source>&amp;Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Im&amp;primeix</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="332"/>
         <source>Ctrl+P</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+P</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="342"/>
         <source>First File</source>
-        <translation type="unfinished"></translation>
+        <translation>Primer fitxer</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="345"/>
         <source>Home</source>
-        <translation type="unfinished"></translation>
+        <translation>Inici</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="355"/>
         <source>Last File</source>
-        <translation type="unfinished"></translation>
+        <translation>Últim fitxer</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="358"/>
         <source>End</source>
-        <translation type="unfinished"></translation>
+        <translation>Fi</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="368"/>
         <source>&amp;New Window</source>
-        <translation type="unfinished"></translation>
+        <translation>Finestra &amp;nova</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="371"/>
         <source>Ctrl+N</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+N</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="376"/>
         <source>Flip &amp;Horizontally</source>
-        <translation type="unfinished"></translation>
+        <translation>Inverteix &amp;horitzontalment</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="386"/>
         <source>Capture Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Pren una captura de pantalla</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="394"/>
         <source>F&amp;ull Screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Pantalla c&amp;ompleta</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="397"/>
         <source>F11</source>
-        <translation type="unfinished"></translation>
+        <translation>F11</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="402"/>
         <source>Flip &amp;Vertically</source>
-        <translation type="unfinished"></translation>
+        <translation>Inverteix &amp;verticalment</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="412"/>
         <source>&amp;Paste from Clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Enganxa del &amp;porta-retalls</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="425"/>
         <source>&amp;Slide Show</source>
-        <translation type="unfinished"></translation>
+        <translation>Presentació de diapo&amp;sitives</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="435"/>
         <source>&amp;Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Suprimeix</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="438"/>
         <source>Del</source>
-        <translation type="unfinished"></translation>
+        <translation>Supr</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="446"/>
         <source>Show Thumbnails</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostra les miniatures</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="451"/>
         <source>File Properties</source>
-        <translation type="unfinished"></translation>
+        <translation>Propietats del fitxer</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="55"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Fitxer</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="71"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Ajuda</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="77"/>
         <source>Go</source>
-        <translation type="unfinished"></translation>
+        <translation>Vés</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="86"/>
         <source>&amp;View</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Vista</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="100"/>
         <source>&amp;Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Edita</translation>
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="120"/>
         <source>Toolbar</source>
-        <translation type="unfinished"></translation>
+        <translation>Barra d&apos;eines</translation>
     </message>
 </context>
 <context>
@@ -329,32 +335,32 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../preferencesdialog.ui" line="14"/>
         <source>Preferences</source>
-        <translation type="unfinished"></translation>
+        <translation>Preferències</translation>
     </message>
     <message>
         <location filename="../../preferencesdialog.ui" line="33"/>
         <source>Icon theme:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tema de les icones:</translation>
     </message>
     <message>
         <location filename="../../preferencesdialog.ui" line="43"/>
         <source>Normal background color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Color del fons normal:</translation>
     </message>
     <message>
         <location filename="../../preferencesdialog.ui" line="50"/>
         <source>Fullscreen background color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Color del fons pantalla completa:</translation>
     </message>
     <message>
         <location filename="../../preferencesdialog.ui" line="71"/>
         <source>Slide show interval (seconds):</source>
-        <translation type="unfinished"></translation>
+        <translation>Interval per mostrar la diapositiva (segons):</translation>
     </message>
     <message>
         <location filename="../../preferencesdialog.ui" line="24"/>
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>General</translation>
     </message>
 </context>
 <context>
@@ -362,47 +368,47 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
     <message>
         <location filename="../../screenshotdialog.ui" line="14"/>
         <source>Screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Captura de pantalla</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="28"/>
         <source>Take a screenshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Pren una captura de pantalla</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="35"/>
         <source>Region</source>
-        <translation type="unfinished"></translation>
+        <translation>Regió</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="41"/>
         <source>Whole screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Tota la pantalla</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="51"/>
         <source>Current window only</source>
-        <translation type="unfinished"></translation>
+        <translation>Tan sols la finestra activa</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="61"/>
         <source>Include mouse cursor</source>
-        <translation type="unfinished"></translation>
+        <translation>Inclou el cursor del ratolí</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="68"/>
         <source> seconds</source>
-        <translation type="unfinished"></translation>
+        <translation> segons</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="85"/>
         <source>Delay:</source>
-        <translation type="unfinished"></translation>
+        <translation>Retard:</translation>
     </message>
     <message>
         <location filename="../../screenshotdialog.ui" line="95"/>
         <source>Include window title and frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Inclou el títol de la finestra i el marc</translation>
     </message>
 </context>
 </TS>
