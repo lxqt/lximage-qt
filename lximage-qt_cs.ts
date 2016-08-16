fb726c2b084787ce7faec930634d41ddf3ec2dc9
Pavel Fric <pavelfric@yahoo.com>
2016-08-16 06:54:50 +0200

    Add/Update Czech translations

---
+++ lximage-qt_cs.ts
@@ -0,0 +1,414 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>LxImage::Application</name>
+    <message>
+        <location filename="../../application.cpp" line="89"/>
+        <source>Take a screenshot</source>
+        <translation>Udělat snímek obrazovky</translation>
+    </message>
+    <message>
+        <location filename="../../application.cpp" line="93"/>
+        <source>[FILE1, FILE2,...]</source>
+        <translation>[SOUBOR1, SOUBOR2,...]</translation>
+    </message>
+</context>
+<context>
+    <name>LxImage::MainWindow</name>
+    <message>
+        <location filename="../../mainwindow.cpp" line="148"/>
+        <source>About</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="149"/>
+        <source>LXImage - a simple and fast image viewer
+
+Copyright (C) 2013
+LXDE Project: http://lxde.org/
+
+Authors:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>LXImage - jednoduchý a rychlý prohlížeč obrázků
+
+Autorské právo (C) 2013
+Projekt LXDE: http://lxde.org/
+
+Autoři:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="242"/>
+        <source>Open File</source>
+        <translation>Otevřít soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="243"/>
+        <location filename="../../mainwindow.cpp" line="264"/>
+        <source>Image files (%1)</source>
+        <translation>Obrázkové soubory (%1)</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="264"/>
+        <source>Save File</source>
+        <translation>Uložit soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="530"/>
+        <source>[*]%1 (Loading...) - Image Viewer</source>
+        <translation>[*]%1 (nahrává se...) - prohlížeč obrázků</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="535"/>
+        <source>[*]%1 (Failed to Load) - Image Viewer</source>
+        <translation>[*]%1 (nepodařilo se nahrát) - prohlížeč obrázků</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="539"/>
+        <source>[*]%1 (%2x%3) - Image Viewer</source>
+        <translation>[*]%1 (%2x%3) - prohlížeč obrázků</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="576"/>
+        <source>[*]Image Viewer</source>
+        <translation>[*] prohlížeč obrázků</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="819"/>
+        <source>Thumbnails</source>
+        <translation>Náhledy</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../mainwindow.ui" line="14"/>
+        <source>Image Viewer</source>
+        <translation>Prohlížeč obrázků</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="159"/>
+        <source>&amp;About</source>
+        <translation>&amp;O programu</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="169"/>
+        <source>&amp;Open File</source>
+        <translation>&amp;Otevřít soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="172"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="182"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Uložit</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="185"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="195"/>
+        <source>Save &amp;As</source>
+        <translation>Uložit &amp;jako</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="198"/>
+        <source>Ctrl+A</source>
+        <translation>Ctrl+A</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="208"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Zavřít</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="211"/>
+        <source>Ctrl+W</source>
+        <translation>Ctrl+W</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="221"/>
+        <source>Zoom &amp;In</source>
+        <translation>&amp;Přiblížit</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="224"/>
+        <source>Ctrl++</source>
+        <translation>Ctrl++</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="234"/>
+        <source>Zoom &amp;Out</source>
+        <translation>&amp;Oddálit</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="237"/>
+        <source>Ctrl+-</source>
+        <translation>Ctrl+-</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="247"/>
+        <source>&amp;Copy to Clipboard</source>
+        <translation>&amp;Kopírovat do schránky</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="257"/>
+        <location filename="../../mainwindow.ui" line="260"/>
+        <source>Next File</source>
+        <translation>Další soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="263"/>
+        <source>PgDown</source>
+        <translation>PgDn</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="273"/>
+        <location filename="../../mainwindow.ui" line="276"/>
+        <source>Previous File</source>
+        <translation>Předchozí soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="279"/>
+        <source>PgUp</source>
+        <translation>PgUp</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="289"/>
+        <source>Original Size</source>
+        <translation>Původní velikost</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="299"/>
+        <source>&amp;Fit</source>
+        <translation>&amp;Přizpůsobit</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="309"/>
+        <source>&amp;Rotate Clockwise</source>
+        <translation>&amp;Otočit po směru hodinových ručiček</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="319"/>
+        <source>Rotate &amp;Counterclockwise</source>
+        <translation>Otočit &amp;proti směru hodinových ručiček</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="324"/>
+        <source>P&amp;references</source>
+        <translation>&amp;Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="329"/>
+        <source>&amp;Print</source>
+        <translation>&amp;Tisk</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="332"/>
+        <source>Ctrl+P</source>
+        <translation>Ctrl+P</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="342"/>
+        <source>First File</source>
+        <translation>První soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="345"/>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="355"/>
+        <source>Last File</source>
+        <translation>Poslední soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="358"/>
+        <source>End</source>
+        <translation>End</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="368"/>
+        <source>&amp;New Window</source>
+        <translation>&amp;Nové okno</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="371"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="376"/>
+        <source>Flip &amp;Horizontally</source>
+        <translation>Převrátit &amp;vodorovně</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="386"/>
+        <source>Capture Screenshot</source>
+        <translation>Udělat snímek obrazovky</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="394"/>
+        <source>F&amp;ull Screen</source>
+        <translation>Na celou &amp;obrazovku</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="397"/>
+        <source>F11</source>
+        <translation>F11</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="402"/>
+        <source>Flip &amp;Vertically</source>
+        <translation>Převrátit &amp;svisle</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="412"/>
+        <source>&amp;Paste from Clipboard</source>
+        <translation>&amp;Vložit ze schránky</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="425"/>
+        <source>&amp;Slide Show</source>
+        <translation>&amp;Promítání snímků</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="435"/>
+        <source>&amp;Delete</source>
+        <translation>S&amp;mazat</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="438"/>
+        <source>Del</source>
+        <translation>Delete</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="446"/>
+        <source>Show Thumbnails</source>
+        <translation>Ukázat náhledy</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="451"/>
+        <source>File Properties</source>
+        <translation>Vlastnosti souboru</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="55"/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="71"/>
+        <source>&amp;Help</source>
+        <translation>Nápo&amp;věda</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="77"/>
+        <source>Go</source>
+        <translation>Pohyb</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="86"/>
+        <source>&amp;View</source>
+        <translation>&amp;Pohled</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="100"/>
+        <source>&amp;Edit</source>
+        <translation>Úp&amp;ravy</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="120"/>
+        <source>Toolbar</source>
+        <translation>Nástrojový pruh</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="14"/>
+        <source>Preferences</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="33"/>
+        <source>Icon theme:</source>
+        <translation>Podoba ikon:</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="43"/>
+        <source>Normal background color:</source>
+        <translation>Barva pozadí v normálním režimu:</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="50"/>
+        <source>Fullscreen background color:</source>
+        <translation>Barva pozadí v režimu na celou obrazovku:</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="71"/>
+        <source>Slide show interval (seconds):</source>
+        <translation>Změna obrázku při promítání snímků (s):</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Obecné</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotDialog</name>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="14"/>
+        <source>Screenshot</source>
+        <translation>Snímek obrazovky</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="28"/>
+        <source>Take a screenshot</source>
+        <translation>Udělat snímek obrazovky</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="35"/>
+        <source>Region</source>
+        <translation>Oblast</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="41"/>
+        <source>Whole screen</source>
+        <translation>Celá obrazovka</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="51"/>
+        <source>Current window only</source>
+        <translation>Pouze nynější okno</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="61"/>
+        <source>Include mouse cursor</source>
+        <translation>Zahrnout ukazovátko myši</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="68"/>
+        <source> seconds</source>
+        <translation> sekund</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="85"/>
+        <source>Delay:</source>
+        <translation>Zpoždění:</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="95"/>
+        <source>Include window title and frame</source>
+        <translation>Zahrnout titulek okna a rámeček</translation>
+    </message>
+</context>
+</TS>
