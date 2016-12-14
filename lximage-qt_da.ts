0aecf29e1d6addc4565ccdeadeff35b4660dd0b0
scootergrisen <scootergrisen@gmail.com>
2016-12-14 02:46:15 +0100

    Update Danish translations

---
+++ lximage-qt_da.ts
@@ -0,0 +1,414 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>LxImage::Application</name>
+    <message>
+        <location filename="../../application.cpp" line="89"/>
+        <source>Take a screenshot</source>
+        <translation>Tag et skærmbillede</translation>
+    </message>
+    <message>
+        <location filename="../../application.cpp" line="93"/>
+        <source>[FILE1, FILE2,...]</source>
+        <translation>[FIL1, FIL2,...]</translation>
+    </message>
+</context>
+<context>
+    <name>LxImage::MainWindow</name>
+    <message>
+        <location filename="../../mainwindow.cpp" line="148"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="149"/>
+        <source>LXImage - a simple and fast image viewer
+
+Copyright (C) 2013
+LXDE Project: http://lxde.org/
+
+Authors:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>LXImage - en simpel og hurtig billedfremviser
+
+Ophavsret (C) 2013
+LXDE-projekt: http://lxde.org/
+
+Forfatter:
+Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="242"/>
+        <source>Open File</source>
+        <translation>Åbn fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="243"/>
+        <location filename="../../mainwindow.cpp" line="264"/>
+        <source>Image files (%1)</source>
+        <translation>Billedfiler (%1)</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="264"/>
+        <source>Save File</source>
+        <translation>Gem fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="530"/>
+        <source>[*]%1 (Loading...) - Image Viewer</source>
+        <translation>[*]%1 (indlæser...) - Billedfremviser</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="535"/>
+        <source>[*]%1 (Failed to Load) - Image Viewer</source>
+        <translation>[*]%1 (kunne ikke indlæse) - Billedfremviser</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="539"/>
+        <source>[*]%1 (%2x%3) - Image Viewer</source>
+        <translation>[*]%1 (%2x%3) - Billedfremviser</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="576"/>
+        <source>[*]Image Viewer</source>
+        <translation>[*]Billedfremviser</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="819"/>
+        <source>Thumbnails</source>
+        <translation>Miniaturer</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../mainwindow.ui" line="14"/>
+        <source>Image Viewer</source>
+        <translation>Billedfremviser</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="159"/>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="169"/>
+        <source>&amp;Open File</source>
+        <translation>&amp;Åbn fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="172"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="182"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Gem</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="185"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="195"/>
+        <source>Save &amp;As</source>
+        <translation>Gem &amp;som</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="198"/>
+        <source>Ctrl+A</source>
+        <translation>Ctrl+A</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="208"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Luk</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="211"/>
+        <source>Ctrl+W</source>
+        <translation>Ctrl+W</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="221"/>
+        <source>Zoom &amp;In</source>
+        <translation>Zoom &amp;ind</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="224"/>
+        <source>Ctrl++</source>
+        <translation>Ctrl++</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="234"/>
+        <source>Zoom &amp;Out</source>
+        <translation>Zoom &amp;ud</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="237"/>
+        <source>Ctrl+-</source>
+        <translation>Ctrl+-</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="247"/>
+        <source>&amp;Copy to Clipboard</source>
+        <translation>&amp;Kopiér til udklipsholder</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="257"/>
+        <location filename="../../mainwindow.ui" line="260"/>
+        <source>Next File</source>
+        <translation>Næste fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="263"/>
+        <source>PgDown</source>
+        <translation>PgDown</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="273"/>
+        <location filename="../../mainwindow.ui" line="276"/>
+        <source>Previous File</source>
+        <translation>Forrige fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="279"/>
+        <source>PgUp</source>
+        <translation>PgUp</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="289"/>
+        <source>Original Size</source>
+        <translation>Original størrelse</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="299"/>
+        <source>&amp;Fit</source>
+        <translation>&amp;Tilpas</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="309"/>
+        <source>&amp;Rotate Clockwise</source>
+        <translation>&amp;Rotér med uret</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="319"/>
+        <source>Rotate &amp;Counterclockwise</source>
+        <translation>Rotér &amp;mod uret</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="324"/>
+        <source>P&amp;references</source>
+        <translation>&amp;Præferencer</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="329"/>
+        <source>&amp;Print</source>
+        <translation>&amp;Udskriv</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="332"/>
+        <source>Ctrl+P</source>
+        <translation>Ctrl+P</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="342"/>
+        <source>First File</source>
+        <translation>Første fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="345"/>
+        <source>Home</source>
+        <translation>Home</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="355"/>
+        <source>Last File</source>
+        <translation>Sidste fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="358"/>
+        <source>End</source>
+        <translation>End</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="368"/>
+        <source>&amp;New Window</source>
+        <translation>&amp;Nyt vindue</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="371"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="376"/>
+        <source>Flip &amp;Horizontally</source>
+        <translation>Vend &amp;vandret</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="386"/>
+        <source>Capture Screenshot</source>
+        <translation>Tag skærmbillede</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="394"/>
+        <source>F&amp;ull Screen</source>
+        <translation>&amp;Fuldskærm</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="397"/>
+        <source>F11</source>
+        <translation>F11</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="402"/>
+        <source>Flip &amp;Vertically</source>
+        <translation>Vend &amp;lodret</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="412"/>
+        <source>&amp;Paste from Clipboard</source>
+        <translation>&amp;Indsæt fra udklipsholder</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="425"/>
+        <source>&amp;Slide Show</source>
+        <translation>&amp;Diasshow</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="435"/>
+        <source>&amp;Delete</source>
+        <translation>&amp;Slet</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="438"/>
+        <source>Del</source>
+        <translation>Del</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="446"/>
+        <source>Show Thumbnails</source>
+        <translation>Vis miniaturer</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="451"/>
+        <source>File Properties</source>
+        <translation>Filegenskaber</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="55"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fil</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="71"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjælp</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="77"/>
+        <source>Go</source>
+        <translation>Gå</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="86"/>
+        <source>&amp;View</source>
+        <translation>&amp;Vis</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="100"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Rediger</translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="120"/>
+        <source>Toolbar</source>
+        <translation>Værktøjslinje</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="14"/>
+        <source>Preferences</source>
+        <translation>Præferencer</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="33"/>
+        <source>Icon theme:</source>
+        <translation>Ikontema:</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="43"/>
+        <source>Normal background color:</source>
+        <translation>Normal baggrundsfarve:</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="50"/>
+        <source>Fullscreen background color:</source>
+        <translation>Baggrundsfarve i fuldskærm:</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="71"/>
+        <source>Slide show interval (seconds):</source>
+        <translation>Diasshow interval (sekunder):</translation>
+    </message>
+    <message>
+        <location filename="../../preferencesdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Generelt</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenshotDialog</name>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="14"/>
+        <source>Screenshot</source>
+        <translation>Skærmbillede</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="28"/>
+        <source>Take a screenshot</source>
+        <translation>Tag et skærmbillede</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="35"/>
+        <source>Region</source>
+        <translation>Område</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="41"/>
+        <source>Whole screen</source>
+        <translation>Hele skærmen</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="51"/>
+        <source>Current window only</source>
+        <translation>Kun aktuelt vindue</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="61"/>
+        <source>Include mouse cursor</source>
+        <translation>Inkluder musemarkør</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="68"/>
+        <source> seconds</source>
+        <translation> sekunder</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="85"/>
+        <source>Delay:</source>
+        <translation>Forsinkelse:</translation>
+    </message>
+    <message>
+        <location filename="../../screenshotdialog.ui" line="95"/>
+        <source>Include window title and frame</source>
+        <translation>Inkluder vinduets titel og ramme</translation>
+    </message>
+</context>
+</TS>
