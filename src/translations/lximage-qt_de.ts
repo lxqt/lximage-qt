cb364bf20deb794584d9465ee4b10c37edd94566
p8q <w45jr6y4ih@bzegi.anonbox.net>
2018-06-22 18:34:12 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (99 of 99 strings)
    
    Translation: LXQt/LXImage-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lximage-qt/de/

--- src/translations/lximage-qt_de.ts
+++ src/translations/lximage-qt_de.ts
@@ -538,7 +538,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     <message>
         <location filename="../upload/uploaddialog.ui" line="14"/>
         <source>Upload</source>
-        <translation type="unfinished"></translation>
+        <translation>Hochladen</translation>
     </message>
 </context>
 </TS>
