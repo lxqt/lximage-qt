c6d3f77b20e496aebcd82c76b52420484771bb27
p-bo <pavel.borecki@gmail.com>
2019-09-09 07:21:47 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (117 of 117 strings)
    
    Translation: LXQt/LXImage-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lximage-qt/cs/

--- src/translations/lximage-qt_cs.ts
+++ src/translations/lximage-qt_cs.ts
@@ -517,7 +517,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     <message>
         <location filename="../mainwindow.ui" line="93"/>
         <source>Go</source>
-        <translation>Jít na</translation>
+        <translation>Přejít na</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="102"/>
@@ -527,7 +527,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     <message>
         <location filename="../mainwindow.ui" line="118"/>
         <source>&amp;Edit</source>
-        <translation>&amp;Upravit</translation>
+        <translation>&amp;Úpravy</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="140"/>
