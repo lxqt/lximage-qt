529f1f802ccfd20baf6173783160a9e72fb07c3d
Dimitrios Glentadakis <dglent@free.fr>
2019-09-29 05:45:40 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (123 of 123 strings)
    
    Translation: LXQt/LXImage-Qt
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lximage-qt/el/

--- src/translations/lximage-qt_el.ts
+++ src/translations/lximage-qt_el.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../application.cpp" line="101"/>
         <source>Take a screenshot to folder without gui</source>
-        <translation type="unfinished"></translation>
+        <translation>Λήψη ενός στιγμιότυπου σε φάκελο χωρίς γραφικό περιβάλλον</translation>
     </message>
     <message>
         <location filename="../application.cpp" line="105"/>
@@ -176,7 +176,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
         <location filename="../mainwindow.ui" line="177"/>
         <location filename="../mainwindow.ui" line="617"/>
         <source>Annotations Toolbar</source>
-        <translation type="unfinished"></translation>
+        <translation>Εργαλειοθήκη σχολιασμού</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="197"/>
@@ -263,7 +263,7 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     <message>
         <location filename="../mainwindow.ui" line="206"/>
         <source>&amp;Open…</source>
-        <translation type="unfinished"></translation>
+        <translation>Ά&amp;νοιγμα…</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="218"/>
@@ -513,18 +513,18 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     <message>
         <location filename="../mainwindow.ui" line="614"/>
         <source>&amp;Annotations Toolbar</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Εργαλειοθήκη σχολιασμού</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="625"/>
         <location filename="../mainwindow.ui" line="628"/>
         <source>Show Outline</source>
-        <translation type="unfinished"></translation>
+        <translation>Προβολή περιγράμματος</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="631"/>
         <source>O</source>
-        <translation type="unfinished"></translation>
+        <translation>O</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="63"/>
@@ -587,12 +587,12 @@ Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     <message>
         <location filename="../preferencesdialog.ui" line="85"/>
         <source>Show image outline by default</source>
-        <translation type="unfinished"></translation>
+        <translation>Προβολή του περιγράμματος εικόνας εξ ορισμού</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="92"/>
         <source>Show annotations toolbar by default</source>
-        <translation type="unfinished"></translation>
+        <translation>Προβολή της εργαλειοθήκης σχολιασμού εξ ορισμού</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="24"/>
