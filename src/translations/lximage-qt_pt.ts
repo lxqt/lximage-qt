1ea4b3387405e2cd9e73a88c2f21d1bb12bc54e3
Sérgio Marques <smarquespt@gmail.com>
2020-11-21 17:06:26 +0100

    Weblate commits
    Co-authored-by: Sérgio Marques <smarquespt@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/pt/
    Translation: LXQt Desktop/LXimage-qt

--- src/translations/lximage-qt_pt.ts
+++ src/translations/lximage-qt_pt.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../application.cpp" line="96"/>
         <source>Take a screenshot (deprecated, please use screengrab instead)</source>
-        <translation type="unfinished"></translation>
+        <translation>Obter captura de ecrã (aplicação descontinuada, utilize screengrab)</translation>
     </message>
     <message>
         <location filename="../application.cpp" line="105"/>
@@ -16,7 +16,7 @@
     <message>
         <location filename="../application.cpp" line="105"/>
         <source>Take a screenshot and save it to the directory without showing the GUI (deprecated, please use screengrab instead)</source>
-        <translation type="unfinished"></translation>
+        <translation>Obter captura de ecrã e guardar para um diretório sem abrir a interface (aplicação descontinuada, utilize screengrab)</translation>
     </message>
     <message>
         <location filename="../application.cpp" line="111"/>
