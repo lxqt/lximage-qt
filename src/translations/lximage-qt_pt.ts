43a79fe666c3f54075c6c5a74875c2c2626e8663
Hugo Carvalho <hugokarvalho@hotmail.com>
2021-03-17 18:07:01 +0100

    Weblate commits
    Co-authored-by: Hugo Carvalho <hugokarvalho@hotmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/pt/
    Translation: LXQt Desktop/LXimage-qt

--- src/translations/lximage-qt_pt.ts
+++ src/translations/lximage-qt_pt.ts
@@ -668,27 +668,27 @@ remover o atalho no modo de edição.</translation>
     <message>
         <location filename="../preferencesdialog.ui" line="109"/>
         <source>Fit images when navigating</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustar imagens ao navegar</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="117"/>
         <source>Thumbnails</source>
-        <translation type="unfinished">Miniaturas</translation>
+        <translation>Miniaturas</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="123"/>
         <source>Image size limit for built-in thumbnailer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Limite de tamanho de imagem para miniatura integrada:</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="130"/>
         <source> MiB</source>
-        <translation type="unfinished"></translation>
+        <translation> MiB</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="146"/>
         <source>Show thumbnails dock by default</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar as miniaturas da doca por predefinição</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="154"/>
