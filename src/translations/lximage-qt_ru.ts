ae5115f7087520580bf1a52c06a9c531b6babbea
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-01-17 08:07:44 +0100

    Weblate commits (#527)
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/ru/
    Translation: LXQt Desktop/LXimage-qt

--- src/translations/lximage-qt_ru.ts
+++ src/translations/lximage-qt_ru.ts
@@ -878,7 +878,7 @@ in the editing mode.</source>
     <message>
         <location filename="../preferencesdialog.ui" line="207"/>
         <source>Smooth images on zooming</source>
-        <translation type="unfinished"></translation>
+        <translation>Сглаживать изображения при масштабировании</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="215"/>
