6e35bc2d37fb165849c7c0142299929a0f45b118
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-04-24 20:03:10 +0200

    Weblate commits (#550)
    Co-authored-by: Gabriel Proença Alves <gproenca42@gmail.com>
    Co-authored-by: Paolo Vigoroso <xpaolox@zoznam.sk>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/pt_BR/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/sk/
    Translation: LXQt Desktop/LXimage-qt

--- src/translations/lximage-qt_sk.ts
+++ src/translations/lximage-qt_sk.ts
@@ -632,7 +632,7 @@ v režime úpravy.</translation>
     <message>
         <location filename="../mainwindow.ui" line="552"/>
         <source>Ctrl+U</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+U</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="560"/>
