5957a847d9fd73a4ea3619c4fc21f8e99f69056b
Weblate <noreply@weblate.org>
2023-01-24 13:11:03 +0100

    Weblate commits
    Co-authored-by: Joan Miró <miro.oro.jm@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/ca/
    Translation: LXQt Desktop/LXimage-qt

--- src/translations/lximage-qt_ca.ts
+++ src/translations/lximage-qt_ca.ts
@@ -597,7 +597,7 @@ una drecera en el mode d&apos;edició.</translation>
     <message>
         <location filename="../mainwindow.ui" line="521"/>
         <source>Del</source>
-        <translation>Supr</translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="529"/>
