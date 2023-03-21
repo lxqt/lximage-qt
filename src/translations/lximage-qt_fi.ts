62fadbc2a4d3c0ebd846e3dd766b568fcc8bafff
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-03-22 04:36:45 +0800

    Weblate commits (#603)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/fi/
    Translation: LXQt Desktop/LXimage-qt
    
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>

--- src/translations/lximage-qt_fi.ts
+++ src/translations/lximage-qt_fi.ts
@@ -360,7 +360,7 @@ muokkaustilassa.</translation>
     <message>
         <location filename="../mainwindow.ui" line="117"/>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Lajittele...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="229"/>
@@ -760,52 +760,52 @@ muokkaustilassa.</translation>
     <message>
         <location filename="../mainwindow.ui" line="739"/>
         <source>&amp;By File Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedosto&amp;nimen mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="742"/>
         <source>By File Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedostonimen mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="750"/>
         <source>By &amp;Modification Time</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Muokkausajan mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="753"/>
         <source>By Modification Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Muokkausajan mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="761"/>
         <source>By C&amp;reation Time</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Luomisajan mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="764"/>
         <source>By Creation Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Luomisajan mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="772"/>
         <source>By File &amp;Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedosto&amp;koon mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="775"/>
         <source>By File Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedostokoon mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="783"/>
         <source>By File &amp;Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedostot&amp;yypin mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="786"/>
         <source>By File Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Tiedostotyypin mukaan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="63"/>
