6d68ff3b2f2c973818d0ce239d75ec616708563c
Weblate <noreply@weblate.org>
2023-05-16 21:11:43 +0200

    Weblate commits
    Co-authored-by: milotype <mail@milotype.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/hr/
    Translation: LXQt Desktop/LXimage-qt

--- src/translations/lximage-qt_hr.ts
+++ src/translations/lximage-qt_hr.ts
@@ -432,7 +432,7 @@ prečaca u modusu uređivanja.</translation>
     <message>
         <location filename="../mainwindow.ui" line="117"/>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Razvrstavanje</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="229"/>
@@ -760,52 +760,52 @@ prečaca u modusu uređivanja.</translation>
     <message>
         <location filename="../mainwindow.ui" line="739"/>
         <source>&amp;By File Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema &amp;imenu datoteka</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="742"/>
         <source>By File Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema imenu datoteka</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="750"/>
         <source>By &amp;Modification Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema &amp;vremenu promjene</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="753"/>
         <source>By Modification Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema vremenu promjene</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="761"/>
         <source>By C&amp;reation Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema vremenu &amp;stvaranja</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="764"/>
         <source>By Creation Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema vremenu stvaranja</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="772"/>
         <source>By File &amp;Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema veličini &amp;datoteke</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="775"/>
         <source>By File Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema veličini datoteke</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="783"/>
         <source>By File &amp;Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema vrs&amp;ti datoteke</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="786"/>
         <source>By File Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Prema vrsti datoteke</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="63"/>
