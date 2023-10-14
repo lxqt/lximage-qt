9204494144bd8b98c0e80208aeb2f607be912909
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-10-14 02:58:53 +0200

    Weblate commits (#636)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/de/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/zh_CN/
    Translation: LXQt Desktop/LXimage-qt
    
    Co-authored-by: digger yu <digger-yu@outlook.com>
    Co-authored-by: sal0max <msal.coding@gmail.com>

--- src/translations/lximage-qt_de.ts
+++ src/translations/lximage-qt_de.ts
@@ -433,7 +433,7 @@ im Bearbeitungsmodus zu löschen.</translation>
     <message>
         <location filename="../mainwindow.ui" line="117"/>
         <source>Sorting</source>
-        <translation type="unfinished"></translation>
+        <translation>Sortierung</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="229"/>
@@ -762,52 +762,52 @@ im Bearbeitungsmodus zu löschen.</translation>
     <message>
         <location filename="../mainwindow.ui" line="739"/>
         <source>&amp;By File Name</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nach Dateiname</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="742"/>
         <source>By File Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Dateiname</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="750"/>
         <source>By &amp;Modification Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach &amp;Änderungszeit</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="753"/>
         <source>By Modification Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Änderungszeit</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="761"/>
         <source>By C&amp;reation Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach E&amp;rstellungszeit</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="764"/>
         <source>By Creation Time</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Erstellungszeit</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="772"/>
         <source>By File &amp;Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Datei$größe</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="775"/>
         <source>By File Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Dateigröße</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="783"/>
         <source>By File &amp;Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Datei&amp;typ</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="786"/>
         <source>By File Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach Dateityp</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="63"/>
