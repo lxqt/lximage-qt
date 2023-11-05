b48f130c40248eb6af6800eebf1a022e9461a292
Weblate <noreply@weblate.org>
2023-11-05 10:22:43 +0100

    Weblate commits
    Co-authored-by: Jan Rolski <wbcwknvstb@proton.me>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/pl/
    Translation: LXQt Desktop/LXimage-qt

--- src/translations/lximage-qt_pl.ts
+++ src/translations/lximage-qt_pl.ts
@@ -927,42 +927,43 @@ w trybie edycji.</translation>
         <location filename="../preferencesdialog.ui" line="190"/>
         <source>May work only with images which have valid color spaces.
 Reload current image to see the effect.</source>
-        <translation type="unfinished"></translation>
+        <translation>Może działać tylko z obrazami, które mają prawidłowe przestrzenie kolorów.
+Wczytaj ponownie bieżący obraz, aby zobaczyć efekt.</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="183"/>
         <source>Color space:</source>
-        <translation type="unfinished"></translation>
+        <translation>Przestrzeń kolorów:</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="195"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Brak</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="200"/>
         <source>sRGB (supported by most monitors)</source>
-        <translation type="unfinished"></translation>
+        <translation>sRGB (obsługiwana przez większość monitorów)</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="205"/>
         <source>Linear sRGB (linear gamma)</source>
-        <translation type="unfinished"></translation>
+        <translation>Liniowa sRGB (liniowa gamma)</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="210"/>
         <source>Adobe RGB</source>
-        <translation type="unfinished"></translation>
+        <translation>Adobe RGB</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="215"/>
         <source>DisplayP3 (wide-gamut screens)</source>
-        <translation type="unfinished"></translation>
+        <translation>DisplayP3 (ekrany szerokogamutowe)</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="220"/>
         <source>ProPhotoRgb</source>
-        <translation type="unfinished"></translation>
+        <translation>ProPhotoRgb</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="256"/>
