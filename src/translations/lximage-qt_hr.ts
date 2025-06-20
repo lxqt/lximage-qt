9cbc1c8b0f98dfebf917441a2f2d2dcee7dce834
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-06-20 23:04:01 +0200

    Weblate commits (#739)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/lximage-qt/hr/
    Translation: LXQt Desktop/LXimage-qt
    
    Co-authored-by: milotype <mail@milotype.de>

--- src/translations/lximage-qt_hr.ts
+++ src/translations/lximage-qt_hr.ts
@@ -190,7 +190,7 @@
         <location filename="../mainwindow.cpp" line="742"/>
         <location filename="../mainwindow.cpp" line="917"/>
         <source>%1/%2 Frames</source>
-        <translation type="unfinished"></translation>
+        <translation>%1/%2 stranice</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="896"/>
@@ -330,7 +330,7 @@ prečaca u modusu uređivanja.</translation>
     <message>
         <location filename="../mainwindow.ui" line="72"/>
         <source>&amp;Recent Files</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Nedavne datoteke</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="214"/>
@@ -801,37 +801,37 @@ prečaca u modusu uređivanja.</translation>
     <message>
         <location filename="../mainwindow.ui" line="817"/>
         <source>Next Frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Sljedeća stranica</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="820"/>
         <source>Ctrl+Right</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Desno</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="825"/>
         <source>Previous Frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Prethodna stranica</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="828"/>
         <source>Ctrl+Left</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Lijevo</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="833"/>
         <source>First Frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Prva stranica</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="838"/>
         <source>Last Frame</source>
-        <translation type="unfinished"></translation>
+        <translation>Zadnja stranica</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="849"/>
         <source>Solid Background</source>
-        <translation type="unfinished"></translation>
+        <translation>Pozadina pune boje</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="63"/>
@@ -1007,7 +1007,7 @@ Za prikaz učinka ponovo učitaj trenutačnu sliku.</translation>
     <message>
         <location filename="../preferencesdialog.ui" line="242"/>
         <source>Solid background by default</source>
-        <translation type="unfinished"></translation>
+        <translation>Standardno se koristi pozadina pune boje</translation>
     </message>
     <message>
         <location filename="../preferencesdialog.ui" line="263"/>
